6b4dfcc4880951b2f823365380126c41e24bb984
CTingCTer <gmiller290488@gmail.com>
2020-08-27 11:47:56 +0100

    Changed some variable names

--- js/indexScript.js
+++ js/indexScript.js
@@ -3,6 +3,7 @@ const cardImages = ['music', 'dog', 'ball', 'videogames'];
 const cardBack = "card-back";
 const cardFront = "card-front";
 const cardCircleImg = "card-circle-img"
+const offWhiteColourString = "#f8f6f6";
 
 function getRandomColour() {
     let r = Math.floor(Math.random() * 256);
@@ -23,12 +24,16 @@ function formatRGBValuesToString(arr) {
     return "rgb(" + arr[0] + "," + arr[1] + "," + arr[2] + ")";
 }
 
-function setColours(gradientColour, linearColour, textColour, textColourSuffix, index) {
+function formatImageUrl(cardImage, textColourString) {
+    return `url('../images/${cardImage}${textColourString}.png')`
+}
+
+function setColours(gradientColour, backgroundColour, textColour, imageUrl, index) {
     document.getElementsByClassName(cardBack)[index].style.background = gradientColour;
     document.getElementsByClassName(cardBack)[index].style.color = textColour;
-    document.getElementsByClassName(cardFront)[index].style.background = linearColour;
-    document.getElementsByClassName(cardFront)[index].style.backgroundImage = `url('../images/${cardImages[index]}${textColourSuffix}.png')`;
-    document.getElementsByClassName(cardCircleImg)[index].style.backgroundImage = `url('../images/${cardImages[index]}${textColourSuffix}.png')`;
+    document.getElementsByClassName(cardFront)[index].style.background = backgroundColour;
+    document.getElementsByClassName(cardFront)[index].style.backgroundImage = imageUrl;
+    document.getElementsByClassName(cardCircleImg)[index].style.backgroundImage = imageUrl;
 }
 
 function generateColours() {
@@ -37,8 +42,8 @@ function generateColours() {
         let formattedBackgroundColour = formatRGBValuesToString(bgColour);
         let textIsWhite = shouldTextBeWhite(bgColour); 
         let textColour = textIsWhite ? formatRGBValuesToString([255, 255, 255]) : formatRGBValuesToString([0, 0, 0]);
-        let formattedGradientBackgroundColour = `linear-gradient(to bottom, ${formattedBackgroundColour} 0%,${formattedBackgroundColour} 30%,${formattedBackgroundColour} 30%, #f8f6f6 30%, #f8f6f6 100%)`;
-        setColours(formattedGradientBackgroundColour, formattedBackgroundColour, textColour, (textIsWhite ? '-white' : '-black'), i)
+        let formattedGradientBackgroundColour = `linear-gradient(to bottom, ${formattedBackgroundColour} 0%, ${formattedBackgroundColour} 30%, ${offWhiteColourString} 30%, ${offWhiteColourString} 100%)`;
+        setColours(formattedGradientBackgroundColour, formattedBackgroundColour, textColour, formatImageUrl(cardImages[i], (textIsWhite ? '-white' : '-black')), i)
     }
 }
 
